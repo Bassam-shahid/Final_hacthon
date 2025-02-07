6b433f4219674c9533f8f8e17722b957156f562f
Bassam Shahid <bassam.shahid01@gmail.com>
2025-02-07 15:55:12 +0500

    Fixed type error in ProductDetail page

--- src/context/CartProvider.tsx
+++ src/context/CartProvider.tsx
@@ -5,11 +5,7 @@ import { CartContext, CartItem, CartContextType } from "./CartContext";
 import { WishlistContext, WishlistItem, WishlistContextType } from "./CartContext";
 import { toast } from "react-toastify";
 
-export const CombinedProvider = ({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) => {
+export const CombinedProvider = ({ children }: { children: React.ReactNode }) => {
   // Cart State
   const [cartItems, setCartItems] = useState<CartItem[]>(() => {
     if (typeof window !== "undefined") {
@@ -40,107 +36,127 @@ export const CombinedProvider = ({
   // Cart Functions
   const addToCart = (item: CartItem) => {
     const adjustedQuantity = item.quantity > 0 ? item.quantity : 1;
+    let toastMessage = "";
+
     setCartItems((prevItems) => {
       const existingItem = prevItems.find((i) => i.id === item.id);
       if (existingItem) {
         const newQuantity = existingItem.quantity + adjustedQuantity;
 
         if (newQuantity > existingItem.stock) {
-          toast.error(`Cannot add more than ${existingItem.stock} items of this product.`, {
-            autoClose: 1000,
-          });
+          toastMessage = `Cannot add more than ${existingItem.stock} items of this product.`;
           return prevItems;
         }
 
         if (newQuantity > 5) {
-          toast.error("Cannot add more than 5 items of the same product.", {
-            autoClose: 1000,
-          });
+          toastMessage = "Cannot add more than 5 items of the same product.";
           return prevItems;
         }
 
-        toast.info("Product Quantity Updated!", { autoClose: 500 });
+        toastMessage = "Product Quantity Updated!";
         return prevItems.map((i) =>
           i.id === item.id ? { ...i, quantity: newQuantity } : i
         );
       }
 
       if (adjustedQuantity > item.stock) {
-        toast.error(`Cannot add more than ${item.stock} items of this product.`, {
-          autoClose: 1000,
-        });
+        toastMessage = `Cannot add more than ${item.stock} items of this product.`;
         return prevItems;
       }
 
       if (adjustedQuantity > 5) {
-        toast.error("Cannot add more than 5 items of the same product.", {
-          autoClose: 1000,
-        });
+        toastMessage = "Cannot add more than 5 items of the same product.";
         return prevItems;
       }
 
-      toast.success("Product Added to Cart!", { autoClose: 500 });
+      toastMessage = "Product Added to Cart!";
       return [...prevItems, { ...item, quantity: adjustedQuantity }];
     });
+
+    if (toastMessage) {
+      setTimeout(() => {
+        toast.success(toastMessage, { autoClose: 500 });
+      }, 0);
+    }
   };
 
   const removeFromCart = (id: string) => {
     setCartItems((prevItems) => prevItems.filter((item) => item.id !== id));
-    toast.info("Product Removed from Cart", { autoClose: 500 });
+    setTimeout(() => {
+      toast.info("Product Removed from Cart", { autoClose: 500 });
+    }, 0);
   };
 
   const clearCart = () => {
     setCartItems([]);
-    toast.info("Cart Cleared!", { autoClose: 500 });
+    setTimeout(() => {
+      toast.info("Cart Cleared!", { autoClose: 500 });
+    }, 0);
   };
 
   const updateQty = (id: string, quantity: number) => {
     const adjustedQuantity = quantity > 0 ? quantity : 1;
+    let toastMessage = "";
+
     setCartItems((prevItems) => {
       const item = prevItems.find((item) => item.id === id);
 
       if (item && adjustedQuantity > item.stock) {
-        toast.error(`Cannot exceed ${item.stock} items of this product.`, {
-          autoClose: 1000,
-        });
+        toastMessage = `Cannot exceed ${item.stock} items of this product.`;
         return prevItems;
       }
 
       if (adjustedQuantity > 5) {
-        toast.error("Cannot have more than 5 items of the same product.", {
-          autoClose: 1000,
-        });
+        toastMessage = "Cannot have more than 5 items of the same product.";
         return prevItems;
       }
 
-      toast.info("Product Quantity Updated!", { autoClose: 500 });
+      toastMessage = "Product Quantity Updated!";
       return prevItems.map((item) =>
         item.id === id ? { ...item, quantity: adjustedQuantity } : item
       );
     });
+
+    if (toastMessage) {
+      setTimeout(() => {
+        toast.info(toastMessage, { autoClose: 500 });
+      }, 0);
+    }
   };
 
   // Wishlist Functions
   const addToWishlist = (item: WishlistItem) => {
+    let toastMessage = "";
+
     setWishlistItems((prevItems) => {
       if (prevItems.find((i) => i.id === item.id)) {
-        toast.info("Product is already in the Wishlist!", { autoClose: 500 });
+        toastMessage = "Product is already in the Wishlist!";
         return prevItems;
       }
 
-      toast.success("Product Added to Wishlist!", { autoClose: 500 });
+      toastMessage = "Product Added to Wishlist!";
       return [...prevItems, item];
     });
+
+    if (toastMessage) {
+      setTimeout(() => {
+        toast.success(toastMessage, { autoClose: 500 });
+      }, 0);
+    }
   };
 
   const removeFromWishlist = (id: string) => {
     setWishlistItems((prevItems) => prevItems.filter((item) => item.id !== id));
-    toast.info("Product Removed from Wishlist", { autoClose: 500 });
+    setTimeout(() => {
+      toast.info("Product Removed from Wishlist", { autoClose: 500 });
+    }, 0);
   };
 
   const clearWishlist = () => {
     setWishlistItems([]);
-    toast.info("Wishlist Cleared!", { autoClose: 500 });
+    setTimeout(() => {
+      toast.info("Wishlist Cleared!", { autoClose: 500 });
+    }, 0);
   };
 
   // Combined Context Values
